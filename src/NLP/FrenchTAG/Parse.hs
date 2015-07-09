002b179fbd0d3196715128d33b565003a1d3f6ef
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2015-07-09 17:53:31 +0200

    Parse info about type and AVMs

--- src/NLP/FrenchTAG/Parse.hs
+++ src/NLP/FrenchTAG/Parse.hs
@@ -7,20 +7,8 @@
 module NLP.FrenchTAG.Parse where
 
 
--- import           Control.Applicative       ((*>), (<$), (<$>), (<*), (<|>))
--- import           Control.Monad             (guard)
---
--- import qualified Data.Attoparsec.Text.Lazy as A
--- import qualified Data.Char                 as C
--- import qualified Data.Either               as E
--- import qualified Data.Map                  as M
--- import           Data.Maybe                (catMaybes)
--- import qualified Data.Text.Lazy            as L
--- import qualified Data.Text.Lazy.IO         as L
-
-
-
-import           Control.Applicative ((*>), (<$>), (<*>))
+import           Control.Applicative ((*>), (<$>), (<*>),
+                        optional, (<|>))
 import           Control.Monad ((<=<))
 
 import qualified Data.Foldable       as F
@@ -28,6 +16,7 @@ import qualified Data.Text           as T
 import qualified Data.Text.Lazy      as L
 import qualified Data.Text.Lazy.IO   as L
 import qualified Data.Tree           as R
+import qualified Data.Map.Strict     as M
 
 import qualified Text.HTML.TagSoup   as TagSoup
 import           Text.XML.PolySoup   hiding (P, Q)
@@ -51,8 +40,47 @@ type Q a = PolySoup.Q (XmlTree L.Text) a
 type Sym = L.Text
 
 
+-- | Attribute.
+type Attr = L.Text
+
+
+-- | Attribute value.
+type Val = L.Text
+
+
+-- | Variable.
+type Var = L.Text
+
+
+-- | Attribute-value matrix.
+type AVM = M.Map Attr (Either Val Var)
+
+
+-- | Non-terminal/node type.
+data Type
+    = Std 
+    | Foot
+    | Anchor
+    | Lex
+    | Other SubType
+    deriving (Show, Eq, Ord)
+
+
+-- | Node subtype (e.g. subst, nadj, whatever they mean...)
+type SubType = L.Text
+
+
+-- | Non-terminal.
+data NonTerm = NonTerm
+    { typ   :: Type
+    , sym   :: Sym
+    , top   :: Maybe AVM
+    , bot   :: Maybe AVM }
+    deriving (Show, Eq, Ord)
+
+
 -- | FrenchTAG tree.
-type Tree = R.Tree Sym
+type Tree = R.Tree NonTerm
 
 
 -------------------------------------------------
@@ -77,17 +105,60 @@ treeQ = named "tree" `joinR` first nodeQ
 
 -- | Node parser.
 nodeQ :: Q Tree
-nodeQ = named "node" `joinR` ( R.Node
-        <$> first symQ
+nodeQ = (named "node" *> attr "type") `join` ( \typTxt -> R.Node
+        <$> first (nonTermQ typTxt)
         <*> every' nodeQ )
 
 
--- | Syntagmatic value finder.
+-- | Non-terminal parser.
+nonTermQ :: L.Text -> Q NonTerm
+nonTermQ typ = joinR (named "narg") $
+    first $ joinR (named "fs") $ do
+        sym <- first symQ
+        top <- optional $ first $ avmQ "top"
+        bot <- optional $ first $ avmQ "bot"
+        return $ NonTerm (parseTyp typ) sym top bot
+
+
+-- | Syntagmatic value parser.
 symQ :: Q Sym
-symQ = joinR (named "narg") $ first $
-       joinR (named "fs") $ first $
-       joinR (named "f" *> hasAttrVal "name" "cat") $ first $
-       node (named "sym" *> attr "value")
+symQ = joinR (named "f" *> hasAttrVal "name" "cat") $
+    first $ node (named "sym" *> attr "value")
+
+
+-- | AVM parser.
+avmQ :: L.Text -> Q AVM
+avmQ name = joinR (named "f" *> hasAttrVal "name" name) $
+    first $ joinR (named "fs") $
+        M.fromList <$> every attrValQ
+
+
+-- | An attribute/value parser.
+attrValQ :: Q (Attr, Either Val Var)
+attrValQ = join (named "f" *> attr "name") $ \atr -> do
+    valVar <- first $ (Left <$> valQ)
+                  <|> (Right <$> varQ)
+    return (atr, valVar)
+
+
+-- | Attribute value parser.
+valQ :: Q Val
+valQ = node $ named "sym" *> attr "value"
+
+
+-- | Attribute variable parser.
+varQ :: Q Var
+varQ = node $ named "sym" *> attr "varname"
+
+
+-- | Type parser.
+parseTyp :: L.Text -> Type
+parseTyp x = case x of
+    "std"       -> Std
+    "lex"       -> Lex
+    "anchor"    -> Anchor
+    "foot"      -> Foot
+    _           -> Other x
 
 
 -- | Parse textual contents of the French TAG XML file.
@@ -103,5 +174,5 @@ readGrammar path = parseGrammar <$> L.readFile path
 
 printGrammar :: FilePath -> IO ()
 printGrammar =
-  let printTree = putStrLn . R.drawTree . fmap L.unpack
+  let printTree = putStrLn . R.drawTree . fmap show
   in mapM_ printTree <=< readGrammar
